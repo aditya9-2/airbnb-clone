1e59275ddbcd3624006f35ab56171f5ba3600a31
Aditya Basak <adityabasak7031@gmail.com>
2026-09-24 16:59:48 +0530

    add fonts

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import localFont from "next/font/local"
+
+export const cereal = localFont({
+    src: "../public/fonts/AirbnbCerealVF.woff2",
+    variable: "--font-sans",
+    display: "swap",
+    weight: "400",
+});
